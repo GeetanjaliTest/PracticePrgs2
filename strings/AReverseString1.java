e8805d0c148dd9887d0d81f0442940c61ff129da
GeetanjaliTest <giitanjalim@gmail.com>
2022-08-12 10:25:41 +0530

    Update AReverseString1.java

--- strings/AReverseString1.java
+++ strings/AReverseString1.java
@@ -232,3 +232,17 @@ public class YReverseWordsInString20 {
             rev= rev+strArr[i]+" ";}
         System.out.println(rev);}}}
 
+public class printFrequency {
+    public static void main(String args[]){
+        String s="HelooChinmai";
+        printFequency(s);
+    }public static void printFequency(String s){
+        Map<Character,Integer> mp= new HashMap<Character,Integer>();
+        for(int i=0; i<s.length();i++){
+            if(mp.containsKey(s.charAt(i))){
+                mp.put(s.charAt(i),mp.get(s.charAt(i))+1);
+            } else {mp.put(s.charAt(i), 1);}}
+        for(int i=0; i<s.length();i++){
+            if(mp.get(s.charAt(i)) != 0){
+                System.out.print(s.charAt(i)+ " "+ mp.get(s.charAt(i)) + " ");
+                mp.put(s.charAt(i), 0);}}}}
